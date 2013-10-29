c86ca95273ce4ccc70ae702c2198b8fdc09c4d30
Olav Frengstad <olav@fwt.no>
2013-10-29 10:40:57 +0530

    Nicer interface to edit parsers integrated with Ace

--- src/js/workbenchNetwork.js
+++ src/js/workbenchNetwork.js
@@ -1,3 +1,5 @@
+/*jshint -W054 */
+
 angular.module('workbenchNetwork', ['ngRoute'])
 	.value('version', '0.2.0')
 	.config(['$routeProvider', function($routeProvider) {
@@ -53,14 +55,74 @@ angular.module('workbenchNetwork', ['ngRoute'])
 			$scope.setResource($routeParams.resource);
 		}
 
+		$scope.jsonErr = true;
 		$scope.net = tmNet.get({id: $routeParams.network, device: "expand"});
 		$scope.net.$promise.then(function(net) {
 			breadcrumbs.setName(1, net.name || net.key);
+
+			$scope.editor = ace.edit("editor");
+			$scope.editor.setTheme("ace/theme/github");
+			$scope.editor.getSession().setMode("ace/mode/javascript");
+			$scope.editor.setValue("return " + $scope.stringify(net));
+			$scope.editor.getSession().setUseSoftTabs(false);
+			$scope.editor.getSession().on("changeAnnotation", function(){
+				var annot = $scope.editor.getSession().getAnnotations();
+				$scope.$apply(function() {
+					$scope.jsonErr = false;
+					for (var key in annot) {
+						if (annot.hasOwnProperty(key)) {
+							$scope.jsonErr = $scope.jsonErr || annot[key].type === 'error';
+						}
+					}
+				});
+			});
 		});
 
+		var extendDeep = function extendDeep(target, source) {
+			for (var prop in source) {
+				if (_.isObject(prop) && prop in target) {
+					extendDeep(target[prop], source[prop]);
+				} else {
+					target[prop] = source[prop];
+				}
+
+				return target;
+			}
+		};
+
+		$scope.stringify = function(net) {
+			var omit = ['devices', 'addr', 'counters', 'meta', ''];
+			var patterns = [[/\\n/g, "\n"], [/\\r/g, "\r"], [/\\t/g, "\t"], 
+				[/"\\u0002/g, ""], [/\\u0003"/g, ""]];
+			return _.reduce(patterns,
+				function(acc, r) {
+					return acc.replace(r[0], r[1]);
+				},
+				JSON.stringify(_.omit(net, omit), function(k, v) {
+						if (k.match(/\$/)) {
+							return undefined;
+						} else if (typeof v === 'function') {
+							return "\02" + v.toString + "\03";
+						} else if (typeof v === 'string' && v.substr(0,8) === "function") {
+							return "\02" + v + "\03";
+						}
+
+						return v;
+					}, '\t'));
+		};
+
 		$scope.update = function(net) {
+			var newnet = new Function($scope.editor.getValue())();
+			net = _.extend(net, extendDeep(newnet, net));
+
+			_.each(net.types, function(v, t) {
+				if (v.parser && typeof v.parser === 'function') {
+					net.types[t].parser = v.parser.toString();
+				}
+			});
+
 			net.$update()
-				.then(function(newnet) {
+				.then(function(net) {
 					$scope.alertBody  = "Network '" + net.name || net.key + "' was updated";
 					$scope.alertClass = "success";
 				});
