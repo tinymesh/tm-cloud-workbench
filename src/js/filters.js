e91c2cf392041455c1520846eaca32b7b3c7cfab
Olav Frengstad <olav@fwt.no>
2014-05-20 17:05:54 +0200

    Fix T14 - Address encoding

--- src/js/filters.js
+++ src/js/filters.js
@@ -6,30 +6,37 @@
 angular.module('workbenchFilters', [])
 	.filter('address', function() {
 		return function(val, opts, bigendian) {
-			var buf = [];
+			var buf;
 			bigendian = bigendian || false;
 
 			switch (opts.encoding || "hex") {
 				case "hex":
-					buf = _.map(
-							_.filter(parseInt(val, 10).toString(16).split(/(..)/),
+					buf = ["00", "00", "00", "00"];
+					_.each(_.filter(parseInt(val, 10).toString(16).split(/(..)/),
 							function(x) { return x !== ''; }),
-						function(x) { return ("0" + x).substr(-2); });
+						function(x) {
+							if (bigendian) {
+								buf.push(("0" + x).substr(-2));
+							} else {
+								buf.unshift(("0" + x).substr(-2));
+							}
+						});
 
-					if (bigendian) {
-						return (buf.reverse().join(":") + ":00:00:00").substr(11);
-					} else {
-						return ("00:00:00:" + buf.join(":")).substr(-11);
-					}
-					break;
+					return (bigendian ? buf.slice(-4, 7) : buf.slice(0, 4)).join(":");
 
 				case "bytes":
-					buf = _.map(
-							_.filter(parseInt(val, 10).toString(16).split(/(..)/),
+					buf = [0, 0, 0, 0];
+					_.each(_.filter(parseInt(val, 10).toString(16).split(/(..)/),
 							function(x) { return x !== ''; }),
-						function(x) { return parseInt(x, 16); });
+						function(x) {
+							if (bigendian) {
+								buf.push(parseInt(x, 16));
+							} else {
+								buf.unshift(parseInt(x, 16));
+							}
+						});
 
-					return (bigendian ? buf.reverse() : buf).join(".");
+					return (bigendian ? buf.slice(-4, 7) : buf.slice(0, 4)).join(".");
 
 				default:
 					return val;
